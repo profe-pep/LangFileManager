3f8bd6375c0b687f8899b0674b38b97f7ab6a682
darc1n <darcinx@gmail.com>
2019-05-05 16:52:59 +0200

    Update LanguageRequest.php
    Using backpack_auth() instead of Auth class.

--- src/app/Http/Requests/LanguageRequest.php
+++ src/app/Http/Requests/LanguageRequest.php
@@ -15,7 +15,7 @@ class LanguageRequest extends CrudRequest
     public function authorize()
     {
         // only allow creates if the user is logged in
-        return \Auth::check();
+        return backpack_auth()->check();
     }
 
     /**
