d406e00859176b94657814d9ea54806a4f4f1a57
Cristian Tabacitu <cristitabacitu@gmail.com>
2020-04-24 07:45:34 +0000

    Apply fixes from StyleCI

--- src/routes/backpack/langfilemanager.php
+++ src/routes/backpack/langfilemanager.php
@@ -6,8 +6,8 @@ Route::group([
     'prefix'     => config('backpack.base.route_prefix', 'admin'),
     'middleware' => ['web', config('backpack.base.middleware_key', 'admin')],
 ], function () {
-        // Language
-        Route::get('language/texts/{lang?}/{file?}', 'LanguageCrudController@showTexts');
-        Route::post('language/texts/{lang}/{file}', 'LanguageCrudController@updateTexts');
-        Route::crud('language', 'LanguageCrudController');
-    });
+    // Language
+    Route::get('language/texts/{lang?}/{file?}', 'LanguageCrudController@showTexts');
+    Route::post('language/texts/{lang}/{file}', 'LanguageCrudController@updateTexts');
+    Route::crud('language', 'LanguageCrudController');
+});
